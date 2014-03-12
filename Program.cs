b3ec484eaf7108f34b2b3d21d197835f513c79ca
enda sexton <endasexton@hotmail.com>
2014-03-12 14:18:34 +0000

    i added things

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Data;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+ 
+
+namespace WebScrappy
+{
+    class Program
+    {                
+        static void Main(){
+             WebClient w = new WebClient();
+             String s = w.DownloadString("http://www.dit.ie");
+             Regex x = new Regex("<[^>]*>");
+             //String target = s.replaceAll("(?i)<td[^>]*>", " ").replaceAll("\\s+", " ").trim();
+             String ss = x.Replace(s, " ");
+             Console.WriteLine(ss);
+             Console.ReadLine();
+
+
+            
+        }
+    }
+}
